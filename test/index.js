03b4e75d0b25133b04beff5e18bfd844bfbfb230
Simon Bugert <simon.bugert@googlemail.com>
2015-03-05 14:13:57 +0100

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,15 @@
+var Code = require('code')
+  , Lab = require('lab')
+  , add = require('../index').add;
+
+var lab = exports.lab = Lab.script();
+
+lab.experiment('Experiment', function() {
+
+  lab.test('Add function', function (done) {
+    var result = add(1, 2);
+
+    Code.expect(result).to.equal(3);
+    done();
+  });
+});
